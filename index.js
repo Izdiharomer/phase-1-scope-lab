2f19dfcf5153ddb3c6fe88850dbfb9b9bad75a60
Izdihar <ezdiharomerrrr@gmail.com>
2023-05-28 12:38:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB'
+}
+
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'John';
+    leastFavoriteCustomer = 'Jane'; 
+  }
